4505d1960baf69fd1995afdadd911ea3216b6dbf
SamuelWade <samuelrcwade@gmail.com>
2023-02-21 14:00:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code in this file!
+let currentUser = 'Sam';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser} !`;
+
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()} !`;
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)} !`;
